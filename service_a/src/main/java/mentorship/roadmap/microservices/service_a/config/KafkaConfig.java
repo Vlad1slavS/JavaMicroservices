c019331c587535350c12050fc7fe6a6e85023aa4
Владислав Степанов <vstepanov@web-bee.ru>
2026-02-26 13:25:15 +0900

    feat: добавил отправку необработанных сообщений в DLT кафки (вместо простого смещения offset)

--- service_a/src/main/java/mentorship/roadmap/microservices/service_a/config/KafkaConfig.java
+++ service_a/src/main/java/mentorship/roadmap/microservices/service_a/config/KafkaConfig.java
@@ -1,8 +1,9 @@
 package mentorship.roadmap.microservices.service_a.config;
 
-import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,11 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 import tools.jackson.core.JacksonException;
@@ -35,6 +40,13 @@ public class KafkaConfig {
     @Value("${spring.kafka.consumer.value-deserializer}")
     private String valueDeserializer;
 
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+
     @Value("${spring.kafka.consumer.auto-offset-reset}")
     private String autoOffsetReset;
 
@@ -51,27 +63,49 @@ public class KafkaConfig {
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
+
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            DefaultErrorHandler errorHandler) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
-
-        factory.setCommonErrorHandler(errorHandler());
+        factory.setCommonErrorHandler(errorHandler);
         return factory;
     }
 
     @Bean
-    public DefaultErrorHandler errorHandler() {
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, String> kafkaTemplate) {
         FixedBackOff backOff = new FixedBackOff(2000L, 4);
-        DefaultErrorHandler handler = new DefaultErrorHandler((record, exception) ->
-                log.error("All attempts have been exhausted. The message will be ignored: {}",
-                        record.value()), backOff);
-        handler.setAckAfterHandle(true);
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(
+                kafkaTemplate,
+                (record, ex) ->
+                        new TopicPartition(record.topic() + ".DLT", record.partition())
+        );
 
-        handler.addNotRetryableExceptions(JacksonException.class);
-        handler.addNotRetryableExceptions(FeignException.FeignClientException.class);
+        DefaultErrorHandler handler = new DefaultErrorHandler(recoverer, backOff);
+
+        handler.addNotRetryableExceptions(
+                JacksonException.class,
+                feign.FeignException.FeignClientException.class
+        );
+
+        handler.setCommitRecovered(true);
         return handler;
     }
 }
